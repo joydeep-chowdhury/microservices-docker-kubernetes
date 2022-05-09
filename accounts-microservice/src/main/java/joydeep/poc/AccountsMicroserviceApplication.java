e08d214a90b9b1c8c6b64d4b72d7a0464ef15ed6
joydeep-chowdhury <joydeepc101@gmail.com>
2022-05-09 18:52:21 +0530

    Pushing some commits

--- accounts-microservice/src/main/java/joydeep/poc/AccountsMicroserviceApplication.java
+++ accounts-microservice/src/main/java/joydeep/poc/AccountsMicroserviceApplication.java
@@ -3,15 +3,14 @@ package joydeep.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-//@ComponentScans({ @ComponentScan("joydeep.poc.controllers") })
-//@EnableJpaRepositories("joydeep.poc.repositories")
-//@EntityScan("joydeep.poc.models")
+@RefreshScope
 public class AccountsMicroserviceApplication {
 
 	public static void main(String[] args) {
--- loans-microservice/src/main/java/joydeep/poc/LoansMicroserviceApplication.java
+++ loans-microservice/src/main/java/joydeep/poc/LoansMicroserviceApplication.java
@@ -3,12 +3,14 @@ package joydeep.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@RefreshScope
 public class LoansMicroserviceApplication {
 
 	public static void main(String[] args) {
