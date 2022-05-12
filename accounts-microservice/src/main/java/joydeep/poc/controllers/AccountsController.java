e60d1bfc8aa4e25de8797cbef4b2a804d7aac0a7
joydeep-chowdhury <joydeepc101@gmail.com>
2022-05-12 09:47:46 +0530

    Pushing some changes

--- accounts-microservice/src/main/java/joydeep/poc/controllers/AccountsController.java
+++ accounts-microservice/src/main/java/joydeep/poc/controllers/AccountsController.java
@@ -11,6 +11,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import joydeep.poc.configurations.AccountsServiceConfig;
 import joydeep.poc.feignclients.CardsFeignClient;
 import joydeep.poc.feignclients.LoansFeignClient;
@@ -50,6 +53,11 @@ public class AccountsController {
 	}
 
 	@PostMapping("/myCustomerDetails")
+	/*
+	 * @CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod =
+	 * "myCustomerDetailsFallBack")
+	 */
+	@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
 	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
@@ -64,6 +72,16 @@ public class AccountsController {
 
 	}
 
+	private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		return customerDetails;
+
+	}
+
 	@GetMapping("/accounts-microservice/config")
 	public String show() throws JsonProcessingException {
 		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
@@ -71,6 +89,16 @@ public class AccountsController {
 				accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
+	}
+	
+	@GetMapping("/sayHello")
+	@RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+	public String sayHello() {
+		return "Hello, Welcome to EazyBank";
+	}
+
+	private String sayHelloFallback(Throwable t) {
+		return "Hi, Welcome to EazyBank";
 	}
 
 }
